4018952830692e5c140c4b8e75b732ad29b9431d
Bassist12345 <129359946+Bassist12345@users.noreply.github.com>
2023-05-14 11:41:54 +0800

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,10 @@
+function show1(){
+    document.getElementById("fb").style.display = "block";
+    document.getElementById("in").style.display = "block";
+    document.getElementById("tw").style.display = "block";
+}
+function hide1(){
+    document.getElementById("fb").style.display = "none";
+    document.getElementById("in").style.display = "none";
+    document.getElementById("tw").style.display = "none";
+}
---
+++ script/shop.js
@@ -0,0 +1,54 @@
+function show1(){
+    document.getElementById("samsungcontainer").style.display = "block";
+    document.getElementById("iphonecontainer").style.display = "none";
+    document.getElementById("xiaomicontainer").style.display = "none";
+    document.getElementById("infinixcontainer").style.display = "none";
+    document.getElementById("huaweicontainer").style.display = "none";
+    document.getElementById("oppocontainer").style.display = "none";
+    document.getElementById("choice").style.display = "none";
+}
+function show2(){
+    document.getElementById("iphonecontainer").style.display = "block";
+    document.getElementById("samsungcontainer").style.display = "none";
+    document.getElementById("xiaomicontainer").style.display = "none";
+    document.getElementById("infinixcontainer").style.display = "none";
+    document.getElementById("huaweicontainer").style.display = "none";
+    document.getElementById("oppocontainer").style.display = "none";
+    document.getElementById("choice").style.display = "none";
+}
+function show3(){
+    document.getElementById("xiaomicontainer").style.display = "block";
+    document.getElementById("samsungcontainer").style.display = "none";
+    document.getElementById("iphonecontainer").style.display = "none";
+    document.getElementById("infinixcontainer").style.display = "none";
+    document.getElementById("huaweicontainer").style.display = "none";
+    document.getElementById("oppocontainer").style.display = "none";
+    document.getElementById("choice").style.display = "none";
+}
+function show4(){
+    document.getElementById("infinixcontainer").style.display = "block";
+    document.getElementById("xiaomicontainer").style.display = "none";
+    document.getElementById("samsungcontainer").style.display = "none";
+    document.getElementById("iphonecontainer").style.display = "none";
+    document.getElementById("huaweicontainer").style.display = "none";
+    document.getElementById("oppocontainer").style.display = "none";
+    document.getElementById("choice").style.display = "none";
+}
+function show5(){
+    document.getElementById("huaweicontainer").style.display = "block";
+    document.getElementById("infinixcontainer").style.display = "none";
+    document.getElementById("xiaomicontainer").style.display = "none";
+    document.getElementById("samsungcontainer").style.display = "none";
+    document.getElementById("iphonecontainer").style.display = "none";
+    document.getElementById("oppocontainer").style.display = "none";
+    document.getElementById("choice").style.display = "none";
+}
+function show6(){
+    document.getElementById("oppocontainer").style.display = "block";
+    document.getElementById("huaweicontainer").style.display = "none";
+    document.getElementById("infinixcontainer").style.display = "none";
+    document.getElementById("xiaomicontainer").style.display = "none";
+    document.getElementById("samsungcontainer").style.display = "none";
+    document.getElementById("iphonecontainer").style.display = "none";
+    document.getElementById("choice").style.display = "none";
+}
